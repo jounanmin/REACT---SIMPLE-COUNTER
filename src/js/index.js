926d621248b1b6cf32add0015088787ddf4d42af
Jonathan <jounanmin@hotmail.com>
2019-10-09 20:37:54 +0000

    initial commit

--- src/js/index.js
+++ src/js/index.js
@@ -8,8 +8,21 @@ import "bootstrap";
 //include your index.scss file into the bundle
 import "../styles/index.scss";
 
+function SimpleCounter(props) {
+	return (
+		<div classsName="BigCounter">
+			<div className="Calendar">
+				<i className="far fa-clock" />
+			</div>
+			<div className="Four" />
+			<div className="Three" />
+			<div className="Two" />
+			<div className="One" />
+		</div>
+	);
+}
 //import your own components
 import { Home } from "./component/home.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<SimpleCounter />, document.querySelector("#app"));
